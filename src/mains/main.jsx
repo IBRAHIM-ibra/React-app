f6118b36fc57047509d3b52d63813cb22ab28a0d
IBRAHIM-ibra <brikoo200411@gmail.com>
2025-05-13 21:56:52 +0100

    not important1

--- src/mains/main.jsx
+++ src/mains/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route,HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import Nav from './Nav.jsx';
 import Apropos from './Apropos.jsx';
@@ -12,20 +12,19 @@ import Footer from './Footer.jsx';
 
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    {/* // <BrowserRouter basename="/React-app"> */}
-    <Nav/>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/Apropos" element={<Apropos />} />
-      <Route path="/Events" element={<Events />} />
-      <Route path="/Projetcs" element={<Projects />} /> 
-      <Route path="/Contact" element={<Contact />} />
-      <Route path="/Join" element={<Join />} />
-    </Routes>
-    <Footer/>
-   {/* </BrowserRouter> */}
-  </HashRouter>
+
+  <BrowserRouter basename="/React-app">
+  <Nav/>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/Apropos" element={<Apropos />} />
+    <Route path="/Events" element={<Events />} />
+    <Route path="/Projetcs" element={<Projects />} /> 
+    <Route path="/Contact" element={<Contact />} />
+    <Route path="/Join" element={<Join />} />
+  </Routes>
+  <Footer/>
+</BrowserRouter>
 );
 
 
