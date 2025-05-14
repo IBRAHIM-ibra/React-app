9f2255cc4fd8999c917d3d7c98fb59bf7e5186c8
IBRAHIM-ibra <brikoo200411@gmail.com>
2025-05-14 17:10:10 +0100

    change the images path in events

--- src/Smains/EvenementsData.jsx
+++ src/Smains/EvenementsData.jsx
@@ -1,4 +1,3 @@
-
 const evenements = [
   {
     id: 1,
@@ -6,7 +5,7 @@ const evenements = [
     date: "2024-2025",
     description:
       "La journée de lancement et d'intégration a permis de présenter le club Enactus FSBM tout en facilitant l'intégration des nouveaux membres...",
-    image: "../images/ClubImages/image0.png",
+    image: "/images/ClubImages/image0.png",  // Correct path
   },
   {
     id: 2,
@@ -14,32 +13,32 @@ const evenements = [
     date: "2024",
     description:
       "La fin d'une aventure, mais le début d'une autre. Nous aurions pu atteindre des sommets...",
-      image: "/src/images/ClubImages/image1.png",
-    },
+    image: "/images/ClubImages/image1.png",  // Correct path
+  },
   {
     id: 3,
     title: "Compétition – Phase des ligues",
     date: "29/06/2024",
     description:
       "En compétition à UIT Kenitra, Enactus FSBM a pris sa place méritée en tête...",
-      image: "/src/images/ClubImages/image2.jpg",
-    },
+    image: "/images/ClubImages/image2.jpg",  // Correct path
+  },
   {
     id: 4,
     title: "Journée 8 mars",
     date: "09/03/2024",
     description:
       "L'événement 'L'innovation au féminin' a mis en lumière le rôle des femmes...",
-      image: "/src/images/ClubImages/image3.jpg",
-    },
+    image: "/images/ClubImages/image3.jpg",  // Correct path
+  },
   {
     id: 5,
     title: "Hackathon",
     date: "15/12/2023 - 17/12/2023",
     description:
       "Notre équipe a participé au Méga Hackathon Panafricain pour développer des idées innovantes...",
-      image: "/src/images/ClubImages/image4.jpg",
-    },
+    image: "/images/ClubImages/image4.jpg",  // Correct path
+  },
 ];
 
 export default evenements;
