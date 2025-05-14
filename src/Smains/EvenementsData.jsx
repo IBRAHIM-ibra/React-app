c2399007de571a26bf11b5c094d42e08c7698901
IBRAHIM-ibra <brikoo200411@gmail.com>
2025-05-14 17:12:20 +0100

    change the images path in events

--- src/Smains/EvenementsData.jsx
+++ src/Smains/EvenementsData.jsx
@@ -1,3 +1,9 @@
+import image0 from "../images/ClubImages/image0.png";
+import image1 from "../images/ClubImages/image1.png";
+import image2 from "../images/ClubImages/image2.jpg";
+import image3 from "../images/ClubImages/image3.jpg";
+import image4 from "../images/ClubImages/image4.jpg";
+
 const evenements = [
   {
     id: 1,
@@ -5,7 +11,7 @@ const evenements = [
     date: "2024-2025",
     description:
       "La journée de lancement et d'intégration a permis de présenter le club Enactus FSBM tout en facilitant l'intégration des nouveaux membres...",
-    image: "/images/ClubImages/image0.png",  // Correct path
+    image: image0,  // Using imported image
   },
   {
     id: 2,
@@ -13,7 +19,7 @@ const evenements = [
     date: "2024",
     description:
       "La fin d'une aventure, mais le début d'une autre. Nous aurions pu atteindre des sommets...",
-    image: "/images/ClubImages/image1.png",  // Correct path
+    image: image1,  // Using imported image
   },
   {
     id: 3,
@@ -21,7 +27,7 @@ const evenements = [
     date: "29/06/2024",
     description:
       "En compétition à UIT Kenitra, Enactus FSBM a pris sa place méritée en tête...",
-    image: "/images/ClubImages/image2.jpg",  // Correct path
+    image: image2,  // Using imported image
   },
   {
     id: 4,
@@ -29,7 +35,7 @@ const evenements = [
     date: "09/03/2024",
     description:
       "L'événement 'L'innovation au féminin' a mis en lumière le rôle des femmes...",
-    image: "/images/ClubImages/image3.jpg",  // Correct path
+    image: image3,  // Using imported image
   },
   {
     id: 5,
@@ -37,8 +43,9 @@ const evenements = [
     date: "15/12/2023 - 17/12/2023",
     description:
       "Notre équipe a participé au Méga Hackathon Panafricain pour développer des idées innovantes...",
-    image: "/images/ClubImages/image4.jpg",  // Correct path
+    image: image4,  // Using imported image
   },
 ];
 
 export default evenements;
+
