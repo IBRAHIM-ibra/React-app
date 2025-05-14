dd5fae3e24c30a9778fc99ca071bf2800fc11651
IBRAHIM-ibra <brikoo200411@gmail.com>
2025-05-14 18:23:07 +0100

    change the images path in events

--- src/Smains/projectData.jsx
+++ src/Smains/projectData.jsx
@@ -1,66 +1,85 @@
+import biogazifyImg from '../images/ProjectImages/biogazify.png';
+import bistrokabbImg from '../images/ProjectImages/bistrokab.png';
+import botabakaImg from '../images/ProjectImages/botaba9a.png';
+import bottle3DImg from '../images/ProjectImages/bottle3d.png';
+import educlassImg from '../images/ProjectImages/educlass.png';
+import enboImg from '../images/ProjectImages/enbo.png';
+import potrinovImg from '../images/ProjectImages/potrinov.png';
+import reskinImg from '../images/ProjectImages/reskin.png';
+import sitautiImg from '../images/ProjectImages/sitauti.png';
+import unirideImg from '../images/ProjectImages/uniride.png';
+
 const projectData = [
-    {
-        id: 1,
-        title: "Biogazify",
-        image: "../images/ProjectImages/biogazify.png",
-        shortDescription: " projet vise à convertir du biogaz issu de déchets organiques en une source d'énergie pratique pour la cuisine. "
-      },
-      {
-        id: 2,
-        title: "Bistrokabb",
-        image: "../images/ProjectImages/bistrokab.png",
-        shortDescription: "le câprier, trésor nutritionnel souvent négligé au Maroc, est mis en lumière par Bistrolabb. En créant des produits alimentaires sains et variés à base de câpres, cette initiative soutient les producteurs locaux, valorise le savoir-faire artisanal et crée des opportunités économiques pour les femmes, tout en ravivant la cuisine marocaine traditionnelle. ",
-      },
-      {
-        id: 3,
-        title: "Botabaka",
-        image: "../images/ProjectImages/botaba9a.png",
-        shortDescription: "Lorsque des coupures inattendues de gaz interrompent vos repas ou douches, cela perturbe votre quotidien. Bota Baقa apporte une solution intelligente avec un capteur qui mesure le niveau de gaz restant et envoie des alertes avant que la bouteille ne soit vide, garantissant ainsi une gestion fluide et sans surprise de vos réserves. ",
-      },
-      {
-        id: 4,
-        title: "Bottle 3D",
-        image: "../images/ProjectImages/bottle3d.png",
-        shortDescription: "projet vise à collecter et traiter les bouteilles en plastique pour les recycler en filaments utilisables dans les imprimantes 3D. ",
-      },
-      {
-        id: 5,
-        title: "Educlass mingle",
-        image: "../images/ProjectImages/educlass.png",
-        shortDescription: "un site web intégrant réseaux sociaux et étude, facilitant l'apprentissage, la révision et la communication simultanément pour les utilisateurs. ",
-      },
-      {
-        id: 6,
-        title: "Enbo",
-        image: "../images/ProjectImages/enbo.png",
-        shortDescription: "propose des boîtes écologiques à trois couches, faites de matériaux recyclés, pour protéger les produits et assurer leur traçabilité.",
-      },
-      {
-        id: 7,
-        title: "Potrinov",
-        image: "../images/ProjectImages/potrinov.png",
-        shortDescription: "POTRINOV réinvente l’art de la poterie en alliant savoir-faire traditionnel et innovation moderne. Avec des créations uniques et un service digital en cours de finalisation, nous offrirons à nos partenaires une visibilité authentique et engageante. Ce projet vise à soutenir un artisanat durable et éco-responsable, tout en promettant une expérience enrichissante pour tous.  ",
-      },
-      {
-        id: 8,
-        title: "Reskin",
-        image: "../images/ProjectImages/reskin.png",
-        shortDescription: "Face au gaspillage alimentaire et à l'impact néfaste des cosmétiques industriels surl’environnement, Reskin transforme ces résidus en cosmétiques durables. En recyclantles déchets alimentaires, l'entreprise réduit l'empreinte carbone et crée des produitsrespectueux de la peau et de la planète, répondant ainsi aux attentes des consommateurs éco-conscients ",
-      },
-      {
-        id: 9,
-        title: "Sitauti ",
-        image: "../images/ProjectImages/sitauti.png",
-        shortDescription: "Une plateforme interactive dédiée à l'autisme, offrant divers moyens de soutien aux autistes, leurs familles et aidants,répondant à un besoin social urgent.  ",
-      },
-      {
-        id: 10,
-        title: "Uniride ",
-        image: "../images/ProjectImages/uniride.png",
-        shortDescription: "une application mobile optimise le transport étudiant au Maroc avec covoiturage adaptatif, promouvant mobilité durable, économique et sécurisée. ",
-      },
- ];
-
-
+  {
+    id: 1,
+    title: "Biogazify",
+    image: biogazifyImg,
+    shortDescription:
+      "Projet visant à convertir du biogaz issu de déchets organiques en une source d'énergie pratique pour la cuisine.",
+  },
+  {
+    id: 2,
+    title: "Bistrokabb",
+    image: bistrokabbImg,
+    shortDescription:
+      "Le câprier, trésor nutritionnel souvent négligé au Maroc, est mis en lumière par Bistrolabb. En créant des produits alimentaires sains et variés à base de câpres, cette initiative soutient les producteurs locaux...",
+  },
+  {
+    id: 3,
+    title: "Botabaka",
+    image: botabakaImg,
+    shortDescription:
+      "Bota Baقa apporte une solution intelligente avec un capteur qui mesure le niveau de gaz restant et envoie des alertes avant que la bouteille ne soit vide.",
+  },
+  {
+    id: 4,
+    title: "Bottle 3D",
+    image: bottle3DImg,
+    shortDescription:
+      "Projet visant à collecter et traiter les bouteilles en plastique pour les recycler en filaments utilisables dans les imprimantes 3D.",
+  },
+  {
+    id: 5,
+    title: "Educlass mingle",
+    image: educlassImg,
+    shortDescription:
+      "Un site web intégrant réseaux sociaux et étude, facilitant l'apprentissage, la révision et la communication simultanément pour les utilisateurs.",
+  },
+  {
+    id: 6,
+    title: "Enbo",
+    image: enboImg,
+    shortDescription:
+      "Propose des boîtes écologiques à trois couches, faites de matériaux recyclés, pour protéger les produits et assurer leur traçabilité.",
+  },
+  {
+    id: 7,
+    title: "Potrinov",
+    image: potrinovImg,
+    shortDescription:
+      "POTRINOV réinvente l’art de la poterie en alliant savoir-faire traditionnel et innovation moderne.",
+  },
+  {
+    id: 8,
+    title: "Reskin",
+    image: reskinImg,
+    shortDescription:
+      "Reskin transforme les résidus alimentaires en cosmétiques durables, respectueux de la peau et de la planète.",
+  },
+  {
+    id: 9,
+    title: "Sitauti",
+    image: sitautiImg,
+    shortDescription:
+      "Une plateforme interactive dédiée à l'autisme, offrant divers moyens de soutien aux autistes, leurs familles et aidants.",
+  },
+  {
+    id: 10,
+    title: "Uniride",
+    image: unirideImg,
+    shortDescription:
+      "Une application mobile qui optimise le transport étudiant au Maroc avec un système de covoiturage adaptatif.",
+  },
+];
 
 export default projectData;
